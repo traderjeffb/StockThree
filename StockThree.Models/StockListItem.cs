8e17392f719c761f7f4c0eed195580c4d5c8c27e
Jeff Beeson <traderjeffb@gmail.com>
2017-12-11 12:54:28 -0500

    added controller and a view

---
+++ StockThree.Models/StockListItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockThree.Models
+{
+    public class StockListItem
+    {
+        public int TransactionId { get; set; }
+        public string Ticker { get; set; }
+        public int Shares { get; set; }
+
+        [Display(Name= "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
