445f67c624ba6462510be7aa39dd2f1d067932be
Jeff Beeson <traderjeffb@gmail.com>
2017-12-14 10:43:14 -0500

    tested all API verbs

--- StockThree.API/Controllers/StockThreeController.cs
+++ StockThree.API/Controllers/StockThreeController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 using Microsoft.AspNet.Identity;
+using StockThree.Models;
 using StockThree.Services;
 
 namespace StockThree.API.Controllers
@@ -12,7 +14,7 @@ namespace StockThree.API.Controllers
     public class StockThreeController : ApiController
     {
         //Get api/stock
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             StockService  stockService = CreateStockService();
             var stocks = stockService.GetStocks();
@@ -28,5 +30,51 @@ namespace StockThree.API.Controllers
             return stockService;
 
         }
+        //get api/stock/single stock retrival
+        public IHttpActionResult Get(int id)
+        {
+            StockService stockService = CreateStockService();
+
+            var stock = stockService.GetStockById(id);
+
+            return Ok(stock);
+        }
+
+        //POST /api/stock
+        public IHttpActionResult Post(StockCreate stock)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateStockService();
+
+            if (!service.CreateStock(stock))
+                return InternalServerError();
+
+            return Ok();
+        }
+        //PUT /api/stock
+        public IHttpActionResult Put(StockEdit stock)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest((ModelState));
+
+            var service = CreateStockService();
+
+            if (!service.UpdateStock(stock))
+                return InternalServerError();
+
+            return Ok();
+        }
+        //DELETE /api/staock
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateStockService();
+
+            if (!service.DeleteStock(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
     }
 }
